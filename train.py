95cc4fea3c0303c5380f861b68bbbef3c91965d7
Huy Phan <huykid1206@gmail.com>
2020-11-17 17:40:58 -0500

    Update hyper parameters

--- train.py
+++ train.py
@@ -10,7 +10,6 @@ from main_module import AnimeModule
 
 
 def main(args):
-    os.environ["CUDA_VISIBLE_DEVICES"] = args.gpus_list
     seed_everything(1)
     logger = WandbLogger(name=args.description, project="anime2selfie")
 
@@ -24,10 +23,10 @@ def main(args):
     trainer = Trainer(
         fast_dev_run=bool(args.dev),
         logger=logger,
-        gpus=-1,
+        gpus=[args.gpus_id],
         deterministic=True,
         weights_summary=None,
-        log_every_n_steps=10,
+        log_every_n_steps=1,
         precision=16,
         max_epochs=args.max_epochs,
         resume_from_checkpoint=path,
@@ -54,11 +53,11 @@ if __name__ == "__main__":
     parser = AnimeDataModule.add_data_specific_args(parser)
 
     # TRAINER args
-    parser.add_argument("--dev", type=int, default=1, choices=[0, 1])
-    parser.add_argument("--gpus_list", type=str, default="3")
-    parser.add_argument("--batch_size", type=int, default=8)
+    parser.add_argument("--dev", type=int, default=0, choices=[0, 1])
+    parser.add_argument("--gpus_id", type=int, default=3)
+    parser.add_argument("--batch_size", type=int, default=20)
     parser.add_argument("--no_workers", type=int, default=8)
-    parser.add_argument("--max_epochs", type=int, default=200)
+    parser.add_argument("--max_epochs", type=int, default=100)
     parser.add_argument("--resume", type=str, default="None")
     args = parser.parse_args()
     main(args)
