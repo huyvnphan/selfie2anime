d3c6bb44639406b28231c54b26010f2b4d329598
Huy Phan <huyvnphan@gmail.com>
2020-11-18 03:23:01 -0500

    update no_workers

--- train.py
+++ train.py
@@ -58,7 +58,7 @@ if __name__ == "__main__":
     parser.add_argument("--dev", type=int, default=0, choices=[0, 1])
     parser.add_argument("--gpu_id", type=int, default=3)
     parser.add_argument("--batch_size", type=int, default=1)
-    parser.add_argument("--no_workers", type=int, default=8)
+    parser.add_argument("--no_workers", type=int, default=16)
     parser.add_argument("--max_epochs", type=int, default=200)
     parser.add_argument("--resume", type=str, default="None")
     args = parser.parse_args()
