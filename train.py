1f8e0efc1f1c8262576ce5dd6b1b61bade1a4ed1
Huy Phan <huyvnphan@gmail.com>
2020-11-18 20:31:26 -0500

    Update batch size

--- train.py
+++ train.py
@@ -58,7 +58,7 @@ if __name__ == "__main__":
     # TRAINER args
     parser.add_argument("--dev", type=int, default=0, choices=[0, 1])
     parser.add_argument("--gpu_id", type=str, default="3")
-    parser.add_argument("--batch_size", type=int, default=2)
+    parser.add_argument("--batch_size", type=int, default=4)
     parser.add_argument("--no_workers", type=int, default=16)
     parser.add_argument("--max_epochs", type=int, default=100)
     parser.add_argument("--resume", type=str, default="None")
